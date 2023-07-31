ad5ee0c05192a3003dd6ca6ae6139f6bcc1b30ea
Lantz <lantzshaw@163.com>
2023-07-31 16:57:59 +0800

    feat: add useContext and useReducer

---
+++ src/context/TodoContext.tsx
@@ -0,0 +1,31 @@
+import React, { FC, ReactElement, ReactNode, createContext, useContext, useState } from 'react'
+
+interface TodoContextProviderProps {
+  children: ReactNode
+}
+
+interface TodoData {
+  id?: number
+  title?: string
+}
+
+interface TodoContextData {
+  list: TodoData[]
+}
+
+const TodoContext = createContext({} as TodoContextData)
+
+const TodoContextProvider: FC<TodoContextProviderProps> = ({ children }): ReactElement => {
+  const [todos, setTodos] = useState<TodoData[]>([])
+
+  const context = {
+    list: todos,
+    length: todos.length,
+  }
+
+  return <TodoContext.Provider value={context}></TodoContext.Provider>
+}
+
+export const useTodo = (): TodoContextData => useContext(TodoContext)
+
+export default TodoContextProvider
